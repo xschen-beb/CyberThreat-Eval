7f31bfaa6ad53e50422deff2ac0e36c5c54d9205
Xuan Feng <xuafeng@microsoft.com>
2024-10-30 16:48:46 -0700

    For feedback: first round

---
+++ atr_pipeline_offline.py
@@ -0,0 +1,37 @@
+from threat_research import threat_research_playground
+# open csv file
+f = open("AutomatedThreatResearch.csv", "r")
+data = f.read()
+data = data.split("\n")
+data.pop(0)
+data.pop()
+out_data = []
+
+num = 0
+output_location = "AgentGenReport/"
+for i in data:
+    num += 1
+    if num > 1:
+        break
+    # split the data by comma
+    print("Processing: ", i)
+    i = i.split(",")
+    # append the link to the links list
+    link = i[0]
+    if link.endswith("/"):
+        link = link[:-1]
+    file_name = output_location + link.split("/")[-1] + ".md"
+    fw = open(file_name, "w")
+    text_output = threat_research_playground(link)
+    print(text_output)
+    fw.write(text_output)
+    fw.close()
+    prefix = "https://microsoftapc-my.sharepoint.com/personal/xuafeng_microsoft_com/Documents/Documents/AutomatedThreatResearch/"
+    out_data.append([link, prefix + file_name])
+
+# write the data to a new csv file
+f = open("AutomatedThreatResearch_out.csv", "w")
+f.write("OriginalLink,Output,Comments\n")
+for i in out_data:
+    f.write(i[0] + "," + i[1] + ',' "\n")
+f.close()
