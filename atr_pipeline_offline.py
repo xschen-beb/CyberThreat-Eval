be37332d8dea41ebf416a38736ed933e2ed2ff9b
Xiangsen Chen (FESCO Adecco Human Resources) <v-xiangschen@microsoft.com>
2024-11-18 15:47:49 +0800

    add thread

--- atr_pipeline_offline.py
+++ atr_pipeline_offline.py
@@ -1,35 +1,31 @@
-from threat_research import threat_research_playground
+import concurrent.futures
 import time
+from new_threat_research import threat_research_playground
 
 # open csv file
-f = open("AutomatedThreatResearch.csv", "r")
+# f = open("AutomatedThreatResearch.csv", "r")
+f = open("1114AutomatedThreatResearch.csv", "r")
+
 data = f.read()
 data = data.split("\n")
 data.pop(0)
 data.pop()
+output_location = "241118_newAgentReport02/"
 out_data = []
+prefix = "https://microsoftapc-my.sharepoint.com/personal/xuafeng_microsoft_com/Documents/Documents/AutomatedThreatResearch/"
 
-num = 0
-output_location = "241111_AgentReport/"
-for i in data:
+# Function to process each link
+def process_link(i):
     link_start_time = time.time()
-    num += 1
-    if num < 13:
-        continue
-    # split the data by comma
     print("Processing: ", i)
     i = i.split(",")
-    # append the link to the links list
     link = i[0]
     if link.endswith("/"):
         link = link[:-1]
-    file_name = output_location + link.split("/")[-1] + "03.md"
-    fw = open(file_name, "w")
+    file_name = output_location + link.split("/")[-1] + ".md"
     text_output = threat_research_playground(link)
-    print(text_output)
-    fw.write(text_output)
-    fw.close()
-    prefix = "https://microsoftapc-my.sharepoint.com/personal/xuafeng_microsoft_com/Documents/Documents/AutomatedThreatResearch/"
+    with open(file_name, "w") as fw:
+        fw.write(text_output)
     out_data.append([link, prefix + file_name])
     link_end_time = time.time()
     link_total_time = link_end_time - link_start_time
@@ -37,9 +33,13 @@ for i in data:
     with open("processing_times.txt", "a+", encoding="utf-8") as time_file:
         time_file.write(f"Total execution time for link {link}: {link_total_time} seconds\n")
 
+# Use ThreadPoolExecutor to process links concurrently, controlling the number of threads
+max_threads = 4
+with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+    executor.map(process_link, data)
+
 # write the data to a new csv file
-f = open("AutomatedThreatResearch_out.csv", "w")
-f.write("OriginalLink,Output,Comments\n")
-for i in out_data:
-    f.write(i[0] + "," + i[1] + ',' "\n")
-f.close()
+with open("AutomatedThreatResearch_out.csv", "w") as f:
+    f.write("OriginalLink,Output,Comments\n")
+    for i in out_data:
+        f.write(i[0] + "," + i[1] + ',' "\n")
